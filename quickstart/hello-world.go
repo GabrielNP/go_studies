f009d3b17019a94ece920bfde1d27069f0c9bee7
gabrielnp <gabrielnp.2010@gmail.com>
2021-03-16 10:00:44 -0300

    refactor: abstract into functions

--- quickstart/hello-world.go
+++ quickstart/hello-world.go
@@ -2,45 +2,14 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"reflect"
 )
 
 func main() {
-	//  Declare variables
-	var name string = "Gab"
-	var age int = 24
-	var version float32 = 1.16
-
-	// Print on terminal
-	fmt.Println("Hello World,", name, ",", age, "years old")
-	fmt.Println("Runing Go on", version, "version")
-
-	// Short variable declaration operator: another way to declare variable
-	country := "Brazil"
-	fmt.Println("country is a variable too:", country)
-
-	// Infer types
-	var city = "Sao Paulo"
-	var simpleFloat = 1.6 // Go always choose float64 inferring variable types
-
-	fmt.Println("Type of city variable is", reflect.TypeOf(city))
-	fmt.Println("Type of simpleFloat variable is", reflect.TypeOf(simpleFloat))
-
-	// User input
-	fmt.Println()
-	fmt.Println("Please, choose an option:")
-	fmt.Println("	1- Start Monitoring")
-	fmt.Println("	2- Show Logs")
-	fmt.Println("	0- Exit program")
-
-	var option int
-	fmt.Scanf("%d", &option)
-	fmt.Println("O comando escolhido foi:", option)
-
-	fmt.Println()
-	fmt.Println("Please, confirm: ")
-
-	fmt.Scan(&option) // Since we've already said option variable must be some integer value, with Scan() function we don't need to say the modifier
+	sayHello()
+	showMenu()
+	option := readChosenOption()
 
 	// If statements
 	if option == 1 {
@@ -62,7 +31,53 @@ func main() {
 		fmt.Println("Showing logs...")
 	case 0:
 		fmt.Println("Quiting...")
+		os.Exit(0)
 	default:
-		fmt.Println("Invalid.")
+		fmt.Println("Invalid!")
+		os.Exit(-1)
 	}
+}
+
+func sayHello() {
+	//  Declare variables
+	var name string = "Gab"
+	var age int = 24
+	var version float32 = 1.16
+
+	// Print on terminal
+	fmt.Println("Hello World,", name, ",", age, "years old")
+	fmt.Println("Runing Go on", version, "version")
+
+	// Short variable declaration operator: another way to declare variable
+	country := "Brazil"
+	fmt.Println("country is a variable too:", country)
+
+	// Infer types
+	var city = "Sao Paulo"
+	var simpleFloat = 1.6 // Go always choose float64 inferring variable types
+
+	fmt.Println("Type of city variable is", reflect.TypeOf(city))
+	fmt.Println("Type of simpleFloat variable is", reflect.TypeOf(simpleFloat))
+}
+
+func readChosenOption() int {
+	// User input
+	var option int
+	fmt.Scanf("%d", &option)
+	fmt.Println("O comando escolhido foi:", option)
+
+	fmt.Println()
+	fmt.Println("Please, confirm: ")
+	fmt.Scan(&option) // Since we've already said option variable must be some integer value, with Scan() function we don't need to say the modifier
+	fmt.Println("O comando escolhido foi:", option)
+
+	return option
+}
+
+func showMenu() {
+	fmt.Println()
+	fmt.Println("Please, choose an option:")
+	fmt.Println("	1- Start Monitoring")
+	fmt.Println("	2- Show Logs")
+	fmt.Println("	0- Exit program")
 }
