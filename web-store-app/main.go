a959eaa8dd2d5c1cce5ef8bc031cc6587722f99c
gabrielnp <gabrielnp.2010@gmail.com>
2021-03-16 22:44:32 -0300

    feat: add interface and provide data to template dinamically

--- web-store-app/main.go
+++ web-store-app/main.go
@@ -1,17 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 )
+
+type Product struct {
+	Name        string
+	Description string
+	Price       float64
+	Amount      int
+}
 
 var templates = template.Must(template.ParseGlob("web-store-app/templates/*.html"))
 
 func main() {
 	http.HandleFunc("/", index)
+	fmt.Println("Listening server at http://localhost:8000")
 	http.ListenAndServe(":8000", nil)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	templates.ExecuteTemplate(w, "Index.html", nil)
+	products := []Product{
+		{Name: "T-shirt", Description: "Blue, very beaultiful", Price: 39.9, Amount: 1},
+		{"Sneakers", "Comfortable", 60, 2},
+		{"Headset", "high-end", 200.54, 1},
+	}
+	templates.ExecuteTemplate(w, "Index", products)
 }
